fd30d0051fa98214bbe9b98be4e4116edf9a3b0b
Lavanya.M <110652162+LavanyaM619@users.noreply.github.com>
2023-10-07 12:14:28 +0530

    Add files via upload

---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'cream',
+      slug: 'cream',
+      category: 'face cream',
+      image: '/images/p1.jpg', // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      //brand: 'Nike',
+      //rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'cream',
+      slug: 'cream',
+      category: 'Body',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      //brand: 'Adidas',
+      //rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'cream',
+      slug: 'cream',
+      category: 'cream',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+     // brand: 'Nike',
+      //rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'cream',
+      slug: 'cream',
+      category: 'eyes',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      //brand: 'Puma',
+      //rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
